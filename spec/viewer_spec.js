8e7a195950eeda66fd769474d6bb7ac3bf221a9b
Peter <peterhsaxton@gmail.com>
2014-04-23 14:41:01 +0100

    Generated svg matrix

--- spec/viewer_spec.js
+++ spec/viewer_spec.js
@@ -4,4 +4,15 @@ describe('Viewer', function(){
       new Viewer('invalid');
     }).toThrow(new Error('No element found'));
   });
+  it('should return the matrix', function(){
+    var element = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    console.log(element.createSVGMatrix());
+    console.log(element.transform);
+    agile = new Viewer('mySVG');
+  });
+  it('should be able to access base transform', function(){
+    affix('svg#test g#inner');
+    var element = document.getElementById('inner');
+    expect(element.transform).toBeDefined();
+  });
 });
--- src/viewer.js
+++ src/viewer.js
@@ -1,7 +1,12 @@
 (function(){
-  Viewer = function(id){
+  function getElement(id){
     var element = document.getElementById(id);
     if (element) { return element; }
     throw 'No element found';
+  }
+  Viewer = function(id){
+    var element = getElement(id);
+    console.log(Object.keys(element));
+    this.transform = element.transform;
   };
 }());
